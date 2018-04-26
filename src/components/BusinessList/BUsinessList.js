e817b7469446ff8a21487d5b4744080ec66ee8fd
Shaohua Ma <shoothaha@gmail.com>
2018-04-26 19:45:17 +1000

    stage 1 of the React project_1

--- src/components/BusinessList/BUsinessList.js
+++ src/components/BusinessList/BUsinessList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './BusinessList.css';
-import Business from '..Business/Business';
+import Business from '../Business/Business';
 
 
 class BusinessList extends React.Component 
@@ -15,7 +15,7 @@ class BusinessList extends React.Component
                 <Business />
                 <Business />
             </div>
-        )
+        );
     }
 }
 
